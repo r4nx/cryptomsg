5ee4cf1018da4e087181c2da2f491dd06a4571c3
r4nx <mod34@yandex.ru>
2019-03-12 22:42:52 +0200

    bump version

--- CryptoMsg.lua
+++ CryptoMsg.lua
@@ -17,7 +17,7 @@
 script_name('CryptoMsg')
 script_author('Ranx')
 script_description('In-game AES encryption')
-script_version('0.2.0')
+script_version('0.3.0')
 
 require 'lib.moonloader'
 require 'lib.aeslua'
